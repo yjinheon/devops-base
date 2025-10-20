561e65820381b85a16c8ca40ba2d883f14bca14b
jinheonyoon <yjinheon@gmail.com>
2025-10-20 09:38:39 +0900

    feat(minikube): loki minikube test

--- k8s/minikube/start_minikube_vbox_rbac.sh
+++ k8s/minikube/start_minikube_vbox_rbac.sh
@@ -6,12 +6,12 @@
 
 minikube start \
   --cpus 2 \
-  --memory 7096 \
+  --memory 4096 \
   --disk-size 20g \
   --driver virtualbox \
-  --profile loki
+  --profile rbac
 
 # 필요한 애드온 활성화
-minikube addons enable ingress --profile loki
+minikube addons enable ingress --profile rbac
 
-echo "minikube started with profile loki"
+echo "minikube started with profile rbac"
